99257052d3d8b666b174d16b949e4a44bf602127
WildBoar <ugo.marchesini@libero.it>
2022-06-24 16:06:08 +0200

    Create Document Folder

--- IS_AP/Epidemics/NPIPlanningForEpidemicControl.py
+++ IS_AP/Epidemics/NPIPlanningForEpidemicControl.py
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 from skopt.space import Space
 from eml.net.reader import keras_reader
+import datetime
 
 # Some parameters
 #S0, I0, R0 = 0.99, 0.01, 0.0
@@ -22,6 +23,12 @@ from eml.net.reader import keras_reader
 
 # knn0 = util.load_ml_model('nn0')
 # knn1 = util.load_ml_model('nn1')
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+pd.set_option('display.colheader_justify', 'center')
+pd.set_option('display.precision', 3)
 
 datafolder = 'C:\Sviluppo\Lab\DATA\DesktopSandBox\IS_AP'
 
@@ -56,26 +63,56 @@ beta_base = 0.35
 budget = 20
 gamma = 1/14
 
+
+# knn1
+starttime = datetime.datetime.now()
+
 sol, closed = util.solve_sir_planning(knn1, npis, S0, I0, R0, beta_base=beta_base, budget=budget,
                                       nweeks=nweeks, tlim=tlim)
+endtime = datetime.datetime.now()
+elapsed = endtime - starttime
+print('elapsed {elapsed}'.format(elapsed=int(elapsed.total_seconds() * 1000)))
+
 print(f'Problem closed: {closed}')
 sol_df = util.sol_to_dataframe(sol, npis, nweeks)
-sol_df
+
+print('sol_df')
+print(sol_df)
 
 beta_sched = sol_df.iloc[:-1]['b']
-util.simulate_SIR_NPI(S0, I0, R0, beta_sched, gamma, steps_per_day=1)
+sim_df = util.simulate_SIR_NPI(S0, I0, R0, beta_sched, gamma, steps_per_day=1)
+print('sim_df')
+print(sim_df)
 
+
+
+
+# knn0
+starttime = datetime.datetime.now()
 sol2, closed2 = util.solve_sir_planning(knn0, npis, S0, I0, R0, beta_base=beta_base, budget=budget,
                                       nweeks=nweeks, tlim=tlim)
+endtime = datetime.datetime.now()
+elapsed = endtime - starttime
+print('elapsed {elapsed}'.format(elapsed=int(elapsed.total_seconds() * 1000)))
 print(f'Problem closed: {closed}')
 sol_df2 = util.sol_to_dataframe(sol2, npis, nweeks)
-sol_df2
+print('sol_df2')
+print(sol_df2)
+
 
 beta_sched2 = sol_df2.iloc[:-1]['b']
-util.simulate_SIR_NPI(S0, I0, R0, beta_sched2, gamma, steps_per_day=1)
+sim_df = util.simulate_SIR_NPI(S0, I0, R0, beta_sched2, gamma, steps_per_day=1)
+print('sim_df 2')
+print(sim_df)
 
+# Brute Force
+starttime = datetime.datetime.now()
 best_S, best_sched = util.solve_sir_brute_force(npis, S0, I0, R0, beta_base, gamma, nweeks, budget)
-best_S
+endtime = datetime.datetime.now()
+elapsed = endtime - starttime
+print('elapsed {elapsed}'.format(elapsed=int(elapsed.total_seconds() * 1000)))
+print('best_S')
+print(best_S)
 
 input('Premi un tasto per uscire ...')
 
