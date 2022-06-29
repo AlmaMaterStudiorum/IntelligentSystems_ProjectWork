f9dcc21564b44d5cf4cfc6f43beeb6ed3df09f5e
WildBoar <ugo.marchesini@libero.it>
2022-06-29 18:11:16 +0200

    Add BruteForce

--- IS_AP/Epidemics/epidemics2.py
+++ IS_AP/Epidemics/epidemics2.py
@@ -1,6 +1,7 @@
 
 #from matplotlib import pyplot as plt
 from glob import glob
+from lib2to3.pgen2.token import NAME
 from util import util
 from scipy.integrate import odeint
 import numpy as np
@@ -13,6 +14,8 @@ import matplotlib.pyplot as plt
 import os
 from os.path import exists
 import datetime
+import gc
+
 
 
 # CONFIG
@@ -42,38 +45,78 @@ nameRegTree='regtree'
 nameNN1='nn1'
 nameNN2='nn2'
 nameNN3='nn3'
-nameNN1HF='{name}.{extension}'.format(name=nameNN1,extension='h5')
-nameNN2HF='{name}.{extension}'.format(name=nameNN2,extension='h5')
-nameNN3HF='{name}.{extension}'.format(name=nameNN3,extension='h5')
 datafolder = 'C:\RepositoryGIT\AlmaMaterStudiorum\IntelligentSystems_ProjectWork\IS_AP\data'
 overwriteModel=False
 modelRegressionTree=[]
+solutionGlobal= {
+    nameRegTree : {},
+    nameNN1 : {},
+    nameNN2 : {},
+    nameNN3 : {}
+}
+
+closedGlobal= {
+    nameRegTree : False,
+    nameNN1 : False,
+    nameNN2 : False,
+    nameNN3 : False
+}
+npis = [
+    util.NPI('masks-indoor', effect=0.75, cost=1),
+    util.NPI('masks-outdoor', effect=0.9, cost=1),
+    util.NPI('dad', effect=0.7, cost=3),
+    util.NPI('bar-rest', effect=0.6, cost=3),
+    util.NPI('transport', effect=0.6, cost=4)
+]
+    
+    
+S0, I0, R0 = 0.99, 0.01, 0.00
+nweeks = 3
+tlim = 3600
+beta_base = 0.35
+budget = 20
+starttime=0
 
 
+def PrintNow():
+    now = datetime.datetime.now()
+    snow = now.strftime("%Y-%m-%d %H:%M:%S")
+    print ("Current date and time : {snow}".format(snow=snow))
+    
+def BeginMethod(methodName):
+    global starttime
+    print('------------------------------------------------------')
+    print('Begin {methodName}'.format(methodName=methodName))
+    PrintNow()
+    starttime = datetime.datetime.now()
 
+def EndMethod(methodName):
+    print('End {methodName}'.format(methodName=methodName))
+    PrintNow()
+    elapsed = datetime.datetime.now() - starttime
+    print('elapsed {elapsed}'.format(elapsed=int(elapsed.total_seconds() * 1000)))
+    print('------------------------------------------------------')
 
 def CreateInput():
-    print('Before generate_SIR_input')
+    BeginMethod('generate_SIR_input')
     global sir_tr_in 
     global sir_ts_in 
     sir_tr_in = util.generate_SIR_input(max_samples=n_tr, mode=modeConfig, seed=seedConfig, normalize=True, max_beta=max_beta)
-    sir_ts_in = util.generate_SIR_input(max_samples=n_ts, mode=modeConfig, seed=seedConfig, normalize=True, max_beta=max_beta)
-    
-    
+    sir_ts_in = util.generate_SIR_input(max_samples=n_ts, mode=modeConfig, seed=seedConfig, normalize=True, max_beta=max_beta)    
     sir_tr_in.head()
-    print('After generate_SIR_input')
+    EndMethod('generate_SIR_input')
 
 def CreateOutput():
-    print('Before generate_SIR_output')
+    BeginMethod('generate_SIR_output')
     global sir_tr_out
     global sir_ts_out
     sir_tr_out = util.generate_SIR_output(sir_tr_in, gammaConfig, tmaxConfig)
     sir_ts_out = util.generate_SIR_output(sir_ts_in, gammaConfig, tmaxConfig)
     sir_tr_out.head()
-    print('After generate_SIR_output')
+    EndMethod('generate_SIR_output')
 
 def BuildTrainPrintModelRegressionTree():
-    print('Start BuildTrainPrintModelRegressionTree')
+    BeginMethod('BuildTrainPrintModelRegressionTree')
     print('Before train')
     global modelRegressionTree
     modelRegressionTree = DecisionTreeRegressor(random_state=44)
@@ -95,59 +138,44 @@ def BuildTrainPrintModelRegressionTree():
     # with open('C:\RepositoryGIT\AlmaMaterStudiorum\IntelligentSystems_ProjectWork\IS_AP\data\fst.svg', 'w') as f:
     #    f.write(ax._repr_svg_())
 
-    print('End BuildTrainPrintModelRegressionTree')
+    EndMethod('BuildTrainPrintModelRegressionTree')
 
 def BuildTrainPrintSaveModelNeuralNetwork(hidden,name): 
-    print('------------------------------------------------------')
-    print('BuildTrainPrintSaveModelNeuralNetwork: {name}'.format(name=name))
+    BeginMethod('BuildTrainPrintSaveModelNeuralNetwork: {name}'.format(name=name))
+    nameHF='{name}.{extension}'.format(name=name,extension='h5')
+    if (not exists(os.path.join(datafolder, nameHF))) or (overwriteModel == True):    
+        modelNeuralNework = util.build_ml_model(input_size=4, output_size=3, hidden=hidden, name='MLP')
+        historyNeuralNework = util.train_ml_model(modelNeuralNework, sir_tr_in, sir_tr_out, verbose=0, epochs=100)
+        util.plot_training_history(historyNeuralNework, figsize=figsize)
+        util.print_ml_metrics(modelNeuralNework, sir_tr_in, sir_tr_out, 'training')
+        util.print_ml_metrics(modelNeuralNework, sir_ts_in, sir_ts_out, 'test')
+        util.save_ml_model_with_winfolder(datafolder,modelNeuralNework, name)
+    else:
+        print('No execution')
     
-    modelNeuralNework = util.build_ml_model(input_size=4, output_size=3, hidden=hidden, name='MLP')
-    historyNeuralNework = util.train_ml_model(modelNeuralNework, sir_tr_in, sir_tr_out, verbose=0, epochs=100)
-    util.plot_training_history(historyNeuralNework, figsize=figsize)
-    util.print_ml_metrics(modelNeuralNework, sir_tr_in, sir_tr_out, 'training')
-    util.print_ml_metrics(modelNeuralNework, sir_ts_in, sir_ts_out, 'test')
 
-    util.save_ml_model_with_winfolder(datafolder,modelNeuralNework, name)
-    print('------------------------------------------------------')
+    EndMethod('BuildTrainPrintSaveModelNeuralNetwork: {name}'.format(name=name))
+      
     
 def BuildTrainPrintSaveModelNeuralNetwork1():
-    if (not exists(os.path.join(datafolder, nameNN1HF))) or (overwriteModel == True):
-        BuildTrainPrintSaveModelNeuralNetwork([8,8,8,8],nameNN1)
+    BuildTrainPrintSaveModelNeuralNetwork([8,8,8,8],nameNN1)
                 
 def BuildTrainPrintSaveModelNeuralNetwork2():
-    if (not exists(os.path.join(datafolder, nameNN2HF))) or (overwriteModel == True):
-        BuildTrainPrintSaveModelNeuralNetwork([16,16,16,16],nameNN2)
+    BuildTrainPrintSaveModelNeuralNetwork([16,16],nameNN2)
 
 def BuildTrainPrintSaveModelNeuralNetwork3():
-    if (not exists(os.path.join(datafolder, nameNN3HF))) or (overwriteModel == True):
-        BuildTrainPrintSaveModelNeuralNetwork([16,16],nameNN3)
+    BuildTrainPrintSaveModelNeuralNetwork([16,16,16,16],nameNN3)
          
 def ExecuteOptimizationProblem(name):
-    print('------------------------------------------------------')
-    print('ExecuteOptimizationProblem: {name}'.format(name=name))
-    npis = [
-        util.NPI('masks-indoor', effect=0.75, cost=1),
-        util.NPI('masks-outdoor', effect=0.9, cost=1),
-        util.NPI('dad', effect=0.7, cost=3),
-        util.NPI('bar-rest', effect=0.6, cost=3),
-        util.NPI('transport', effect=0.6, cost=4)
-    ]
-    
-    
-    S0, I0, R0 = 0.99, 0.01, 0.00
-    nweeks = 3
-    tlim = 800
-    beta_base = 0.35
-    budget = 20
-    
-    starttime = datetime.datetime.now()
-    
+    BeginMethod('ExecuteOptimizationProblem: {name}'.format(name=name))
+    gc.collect()
     if name==nameRegTree :
         model = modelRegressionTree
     else:
         model = util.load_ml_model_with_winfolder(datafolder,name)
     
     sol, closed = util.solve_sir_planning(model, npis, S0, I0, R0, beta_base=beta_base, budget=budget,nweeks=nweeks, tlim=tlim)
+       
     cost = 0
     if closed == True:
         cost = sol['cost']
@@ -157,14 +185,12 @@ def ExecuteOptimizationProblem(name):
         print(sol_df)
     else:
         cost = 0
-    endtime = datetime.datetime.now()
-    elapsed = endtime - starttime
-    print('elapsed {elapsed}'.format(elapsed=int(elapsed.total_seconds() * 1000)))
-
+        
+    solutionGlobal[name] = sol_df
+    closedGlobal[name] = closed
+    gc.collect()       
     print(f'Problem closed: {closed} , Cost = {cost}')
-    
-
-    print('------------------------------------------------------')
+    EndMethod('ExecuteOptimizationProblem: {name}'.format(name=name))
  
 def ExecuteOptimizationProblemRegTree():
     ExecuteOptimizationProblem(nameRegTree)
@@ -177,9 +203,44 @@ def ExecuteOptimizationProblemNN2():
     
 def ExecuteOptimizationProblemNN3():
     ExecuteOptimizationProblem(nameNN3)
-    
-def Run(): 
 
+def SimulateProblemByName(name):
+    if closedGlobal[name] == True:
+        SimulateProblem(name,solutionGlobal[name],S0,I0,R0,gammaConfig)
+
+
+def SimulateProblem(name,sol,s,i,r,g):
+    BeginMethod('SimulateProblem {name}'.format(name=name))
+    sim = util.simulate_SIR_NPI(s, i, r, sol.iloc[:-1]['b'], g, steps_per_day=1)
+    print('sim')
+    pd.set_option('display.max_columns',20)
+    print(sim)
+    EndMethod('SimulateProblem {name}'.format(name=name))
+ 
+def SimulateProblemRegTree():
+    SimulateProblemByName(nameRegTree)
+    
+def SimulateProblemNN1():
+    SimulateProblemByName(nameNN1)
+    
+def SimulateProblemNN2():
+    SimulateProblemByName(nameNN2)
+    
+def SimulateProblemNN3():
+    SimulateProblemByName(nameNN3)
+
+def BruteForce():
+    BeginMethod('BruteForce')
+
+    best_S, best_sched = util.solve_sir_brute_force(npis, S0, I0, R0, beta_base, gammaConfig, nweeks, budget)
+
+    print('best_S')
+    print(best_S)
+    EndMethod('BruteForce')
+    
+        
+def Run(): 
+    
     # CREATE INPUT
     CreateInput()
 
@@ -203,15 +264,6 @@ def Run():
     # BUILD + TRAIN + PRINT + SAVE MODEL NN3
     BuildTrainPrintSaveModelNeuralNetwork3()
 
-
-    # DEFINE CONSTRAINS
-
-
-
-    # DEFINE OPTIZATION MODEL 
-
-
-
     # EXECUTE OPTIMIZATION MODEL Regression Tree
     # ExecuteOptimizationProblemRegTree()
     
@@ -226,12 +278,19 @@ def Run():
 
 
     # SIMULATE MODEL Regression Tree
-
+    SimulateProblemRegTree()
+    
     # SIMULATE MODEL NN1
+    SimulateProblemNN1()
 
     # SIMULATE MODEL NN2
+    SimulateProblemNN2()
 
-    
+    # SIMULATE MODEL NN2
+    SimulateProblemNN3()
+
+    # BRUTE FORCE
+    BruteForce()
 
 # START    
 Run()
