e72e119a12eda514025f490347576c1e43b65100
WildBoar <ugo.marchesini@libero.it>
2022-07-21 10:59:00 +0200

    Stop Tracking specificenv.py

--- IS_AP/TransprecisionComputing/specificenv.py
+++
@@ -1,3 +0,0 @@
-datafolder='D:\Sviluppo\GitHub\AlmaMaterStudiorum\IntelligentSystems_ProjectWork\IS_AP\data'
-dataInputFolder=r'D:\Sviluppo\GitHub\AlmaMaterStudiorum\IntelligentSystems_ProjectWork\IS_AP\data\input\tpc'
-dataOutputFolder=r'D:\Sviluppo\GitHub\AlmaMaterStudiorum\IntelligentSystems_ProjectWork\IS_AP\data\output\tpc'
